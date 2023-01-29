87661769c4f2748ea77dca317ed97e858d2c2f8f
sergi <sergicast@hotmail.com>
2023-01-30 00:41:57 +0100

    create a mocked server

---
+++ api/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const { env: { PORT } } = process;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/getMinMaxValues', (req, res) => {
+    const data = { min: 1, max: 100 };
+    res.status(200).json(data);
+});
+app.get('/getStaticValues', (req, res) => {
+    const data = [1.99, 5.99, 10.99, 30.99, 50.99, 70.99];
+    res.status(200).json(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
