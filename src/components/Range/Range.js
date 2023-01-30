3e0851533bf51995f4145974c88c5bb4c1353394
sergi <sergicast@hotmail.com>
2023-01-30 20:34:14 +0100

    update range component for static ranges

--- src/components/Range/Range.js
+++ src/components/Range/Range.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import './Range.scss';
 
-import { getProgressStyles, findStaticRange } from '../../utils';
+import { getProgressStyles, findStaticRange, getPixelsToMove, getSliderProgress } from '../../utils';
 
 export const Range = ({ range, staticRange = [] }) => {
 
@@ -21,113 +21,96 @@ export const Range = ({ range, staticRange = [] }) => {
     }, [leftProgess, rightProgress]);
 
     useEffect(() => {
-        const { min, max } = range;
-        setLeftProgress(((leftValue - min) / (max - min)) * 100);
-        setRightProgress(((rightValue - min) / (max - min)) * 100);
+        setLeftProgress(getSliderProgress(leftValue, range));
+        setRightProgress(getSliderProgress(rightValue, range));
     }, [leftValue, rightValue]);
 
     const getPositionAndProgress = (clientX, type) => {
         const { left, right } = inputRangeRef.current.getBoundingClientRect();
         const convertionNum = (range.max - range.min) / (right - left);
         const progress = ((clientX - left) * convertionNum) + range.min;
-        if (type === 'left') {
-            if (staticRange.length > 0) {
-                const value = progress < range.min ? range.min : progress >= rightValue ? rightValue - 1 : progress;
-                const selectedRange = findStaticRange(staticRange, value);
-                setLeftValue(selectedRange);
-                if (selectedRange) {
-                    debugger
-                    let addPx
-                    if (selectedRange === range.min) {
-                        addPx = 0
-
-                    } else {
-                        addPx = ((right - left) / (range.max - range.min)) * (selectedRange - range.min)
-                    }
-
-                    sliderRefLeft.current.style.left = addPx + "px";
-                    document.current.style.cursor = 'grabbing';
-                    console.log(findStaticRange(staticRange, value))
-
-                }
-            } else {
-                setLeftValue(progress < range.min ? range.min : progress >= rightValue ? rightValue - 1 : progress);
-            }
-        } else {
-            setRightValue(progress > range.max ? range.max : progress <= leftValue ? leftValue + 1 : progress);
-        }
         return { left, right, progress };
     };
 
-    const onMouseMoveLeft = ({ clientX }) => {
-        const { left, right, progress } = getPositionAndProgress(clientX, 'left')
 
-        if (clientX > left && clientX < right && progress < rightValue) {
-            console.log(leftValue)
-            if (staticRange.length > 0) {
-                // const value = progress < range.min ? range.min : progress >= rightValue ? rightValue - 1 : progress;
-                // findStaticRange(staticRange, value)
-                // let currentX = (range.max - range.min) / left;
-                // let addPx = (clientX - left) * currentX
-                // if (leftValue === 10.99) console.log('hola')
-                // sliderRefLeft.current.style.left = addPx + "px";
-            } else {
-                let currentX = clientX - left;
-                sliderRefLeft.current.style.left = (currentX - 5) + "px";
+    const onMouseMove = ({ clientX }, isLeftSlider) => {
+        const { min, max } = range;
+        const { left, right, progress } = getPositionAndProgress(clientX);
+
+        let value;
+        let sliderRef = isLeftSlider ? sliderRefLeft : sliderRefRight;
+        let setValue = isLeftSlider ? setLeftValue : setRightValue;
+
+        if (isLeftSlider) {
+            value = progress < min ? min : progress >= rightValue ? rightValue - 1 : progress;
+        } else {
+            value = progress > max ? max : progress <= leftValue ? leftValue + 1 : progress;
+        }
+
+        if (staticRange.length > 0) {
+            const selectedRange = findStaticRange(staticRange, value);
+            if (selectedRange) {
+                setValue(selectedRange);
+                const pixelsToMove = isLeftSlider ?
+                    getPixelsToMove(range, selectedRange, left, right).leftPixelsAdd - 12 :
+                    getPixelsToMove(range, selectedRange, left, right).rightPixelsAdd;
+                sliderRef.current.style.left = pixelsToMove + "px";
+                sliderRef.current.style.cursor = 'grabbing';
+            }
+        } else {
+            setValue(value);
+            const isLinesCross = isLeftSlider ? (progress < rightValue) : (progress > leftValue);
+            if (clientX > left && clientX < right && isLinesCross) {
+                let currentX = isLeftSlider ? clientX - left : (clientX) - right;
+                sliderRef.current.style.left = (currentX + (isLeftSlider ? -5 : 8)) + "px";
             }
         }
     };
-    const onMouseMoveRight = ({ clientX }) => {
-        const { left, right, progress } = getPositionAndProgress(clientX);
 
-        if (clientX > left && clientX < right && progress > leftValue) {
-            let currentX = (clientX) - right;
-            sliderRefRight.current.style.left = (currentX + 8) + "px";
-        }
-    };
-    const onSetNewValueLeft = (value) => {
+    /**
+     * Modify the position of the slider with input
+     */
+    const onSetNewValue = (value, isLeft) => {
         const { min, max } = range;
-        if (value.length === 0) setLeftValue(value);
+        if (value.length === 0) {
+            isLeft ? setLeftValue(value) : setRightValue(value);
+        }
 
         const { left, right } = inputRangeRef.current.getBoundingClientRect();
         const convertionNum = (right - left) / (max + 1);
-        const pxAddSlider = convertionNum * (parseInt(value) - min);
+        const pxAdd = convertionNum * (parseInt(value) - (isLeft ? min : max));
 
-        if (value >= min && value < max && parseInt(value) < parseInt(rightValue)) {
-            setLeftValue(value);
-            sliderRefLeft.current.style.left = pxAddSlider + "px";
+        if (
+            value >= min &&
+            value <= max &&
+            (isLeft ? parseInt(value) < parseInt(rightValue) : parseInt(value) > parseInt(leftValue))
+        ) {
+            isLeft ? setLeftValue(value) : setRightValue(value);
+            isLeft ? (sliderRefLeft.current.style.left = pxAdd + "px") : (sliderRefRight.current.style.left = pxAdd + "px");
         }
     };
-    const onSetNewValueRight = (value) => {
-        const { min, max } = range;
-        if (value.length === 0) setRightValue(value);
 
-        const { left, right } = inputRangeRef.current.getBoundingClientRect();
-        const convertionNum = (right - left) / (max + 1);
-        const pxAdd = convertionNum * (parseInt(value) - max);
+    const onMoveListenerLeft = (e) => onMouseMove(e, true);
+    const onMoveListenerRight = (e) => onMouseMove(e, false);
+    const onMouseUpListener = (e) => onMouseUp(e, (isSlidingLeft ? 'left' : 'right'));
 
-        if (value >= min && value <= max && parseInt(value) > parseInt(leftValue)) {
-            setRightValue(value);
-            sliderRefRight.current.style.left = pxAdd + "px";
-        }
-    };
     /**
      * Left and Right onMouseUp
      */
     const onMouseUp = (e, slider) => {
         if (slider === 'left') {
             setIsSlidingLeft(false);
-            document.removeEventListener("mousemove", onMouseMoveLeft);
+            document.removeEventListener("mousemove", onMoveListenerLeft);
         } else {
             setIsSlidingRight(false);
-            document.removeEventListener("mousemove", onMouseMoveRight);
+            document.removeEventListener("mousemove", onMoveListenerRight);
         }
     };
 
     useEffect(() => {
         if (isSlidingLeft || isSlidingRight) {
-            document.addEventListener("mousemove", isSlidingLeft ? onMouseMoveLeft : onMouseMoveRight);
-            document.addEventListener("mouseup", (e) => onMouseUp(e, (isSlidingLeft ? 'left' : 'right')));
+            document.addEventListener("mousemove", isSlidingLeft ? onMoveListenerLeft : onMoveListenerRight);
+            document.addEventListener("mouseup", onMouseUpListener);
         }
     }, [isSlidingLeft, isSlidingRight]);
 
@@ -139,38 +122,41 @@ export const Range = ({ range, staticRange = [] }) => {
                     className="range__sliderleft"
                     ref={sliderRefLeft}
                     onMouseDown={(e) => {
-                        console.log('down')
                         setIsSlidingRight(false);
                         setIsSlidingLeft(true);
                         sliderRefLeft.current.style.cursor = 'grabbing';
-                    }
-                    }
-                    onMouseUp={(e) => {
-                        console.log('up')
-                        sliderRefLeft.current.style.cursor = 'grab';
                     }}
+                    onPointerDown={(e) => {
+                        setIsSlidingRight(false);
+                        setIsSlidingLeft(true);
+                        sliderRefLeft.current.style.cursor = 'grabbing';
+                    }}
+                    onMouseUp={(e) => sliderRefLeft.current.style.cursor = 'grab'}
+                    onPointerUp={(e) => sliderRefLeft.current.style.cursor = 'grab'}
+                    
                 >
                 </div>
                 <div
                     className="range__sliderright"
                     ref={sliderRefRight}
                     onMouseDown={(e) => {
-                        console.log('down')
                         setIsSlidingLeft(false);
                         setIsSlidingRight(true);
                         sliderRefRight.current.style.cursor = 'grabbing';
-                    }
-                    }
-                    onMouseUp={(e) => {
-                        console.log('up')
-                        sliderRefRight.current.style.cursor = 'grab'
                     }}
+                    onMouseUp={(e) => sliderRefRight.current.style.cursor = 'grab'}
+                    onPointerDown={(e) => {
+                        setIsSlidingLeft(false);
+                        setIsSlidingRight(true);
+                        sliderRefRight.current.style.cursor = 'grabbing';
+                    }}
+                    onPointerUp={(e) => sliderRefRight.current.style.cursor = 'grab'}
                 >
                 </div>
             </div>
             <div className="range__values">
                 <input
-                    onChange={({ target: { value } }) => onSetNewValueLeft(value)}
+                    onChange={({ target: { value } }) => onSetNewValue(value, true)}
                     value={leftValue}
                     type="number"
                     disabled={staticRange.length > 0}
@@ -178,7 +164,7 @@ export const Range = ({ range, staticRange = [] }) => {
                 <span>-</span>
                 <input
                     value={rightValue}
-                    onChange={({ target: { value } }) => onSetNewValueRight(value)}
+                    onChange={({ target: { value } }) => onSetNewValue(value, false)}
                     type="number"
                     disabled={staticRange.length > 0}
                 />
